21ff1aa4192d2dd8ee33ea03be45e3e630d0fd27
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-07-30 15:46:22 +0200

    rework translation key gathering

--- 1.21.8/Common/src/main/java/fuzs/tooltipinsights/api/v1/client/handler/TooltipDescriptionsHandler.java
+++ 1.21.8/Common/src/main/java/fuzs/tooltipinsights/api/v1/client/handler/TooltipDescriptionsHandler.java
@@ -4,9 +4,11 @@ import fuzs.puzzleslib.api.client.event.v1.entity.player.ClientPlayerNetworkEven
 import fuzs.tooltipinsights.api.v1.client.gui.tooltip.DescriptionLines;
 import fuzs.tooltipinsights.api.v1.config.ItemDescriptionMode;
 import fuzs.tooltipinsights.impl.TooltipInsights;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.core.Registry;
 import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
@@ -17,62 +19,77 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import org.apache.commons.lang3.mutable.MutableBoolean;
+import org.apache.commons.lang3.mutable.MutableInt;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.function.Consumer;
+import java.util.function.BiPredicate;
 import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.function.UnaryOperator;
 
 public abstract class TooltipDescriptionsHandler<T> {
 
     public void onItemTooltip(ItemStack itemStack, List<Component> tooltipLines, Item.TooltipContext tooltipContext, @Nullable Player player, TooltipFlag tooltipFlag) {
+        this.modifyTooltip(itemStack, tooltipLines, tooltipContext.registries(), tooltipFlag);
+    }
+
+    public void onGatherTooltipComponents(Minecraft minecraft, List<Component> tooltipLines) {
+        this.modifyTooltip(ItemStack.EMPTY,
+                tooltipLines,
+                minecraft.getConnection().registryAccess(),
+                minecraft.options.advancedItemTooltips ? TooltipFlag.ADVANCED : TooltipFlag.NORMAL);
+    }
+
+    private void modifyTooltip(ItemStack itemStack, List<Component> tooltipLines, HolderLookup.Provider registries, TooltipFlag tooltipFlag) {
         ItemDescriptionMode itemDescriptionMode = this.getItemDescriptionMode();
 
         if (itemDescriptionMode == ItemDescriptionMode.NEVER && !itemDescriptionMode.isActive()) {
             return;
         }
 
-        Map<String, T> descriptionIds = this.getByDescriptionId(itemStack);
+        Map<String, T> descriptionIds = this.getByDescriptionId(itemStack, registries);
 
         if (!descriptionIds.isEmpty()) {
             MutableBoolean mutableBoolean = new MutableBoolean(true);
 
-            for (int i = 0; i < tooltipLines.size(); i++) {
-                TranslatableContents contents = getTranslatableContents(tooltipLines.get(i));
+            for (MutableInt mutableInt = new MutableInt();
+                 mutableInt.intValue() < tooltipLines.size(); mutableInt.increment()) {
 
-                if (contents != null && descriptionIds.containsKey(contents.getKey())) {
-                    T t = descriptionIds.get(contents.getKey());
-                    Component component = this.getValueComponent(t);
+                modifyTranslatableContents(tooltipLines.get(mutableInt.intValue()),
+                        UnaryOperator.identity(),
+                        (TranslatableContents translatableContents, UnaryOperator<Component> componentReplacer) -> {
 
-                    if (component != null) {
-                        tooltipLines.set(i, component);
-                    }
+                            if (descriptionIds.containsKey(translatableContents.getKey())) {
+                                T t = descriptionIds.get(translatableContents.getKey());
+                                Component component = this.getValueComponent(t);
 
-                    if (itemDescriptionMode.isActive()) {
-                        List<Component> list = this.getItemTooltipLines(t);
-                        tooltipLines.addAll(i + 1, list);
-                        i += list.size();
-                    } else if (mutableBoolean.isTrue()) {
-                        // make sure the view description line is only added when there will actually be a description
-                        mutableBoolean.setFalse();
-                        itemDescriptionMode.processTooltipLines(itemStack, tooltipLines, tooltipFlag);
+                                if (component != null) {
+                                    tooltipLines.set(mutableInt.intValue(), componentReplacer.apply(component));
+                                }
 
-                        // if vanilla tooltip lines are not replaced, we can terminate early
-                        if (component == null) {
-                            break;
-                        }
-                    }
-                }
+                                if (itemDescriptionMode.isActive()) {
+                                    List<Component> list = this.getItemTooltipLines(t);
+                                    tooltipLines.addAll(mutableInt.intValue() + 1, list);
+                                    mutableInt.add(list.size());
+                                    return true;
+                                } else if (mutableBoolean.isTrue()) {
+                                    // make sure the view description line is only added when there will actually be a description
+                                    mutableBoolean.setFalse();
+                                    itemDescriptionMode.processTooltipLines(itemStack, tooltipLines, tooltipFlag);
+                                    return true;
+                                }
+                            }
+
+                            return false;
+                        });
             }
         }
     }
 
     protected abstract ItemDescriptionMode getItemDescriptionMode();
 
-    protected abstract Map<String, T> getByDescriptionId(ItemStack itemStack);
+    protected abstract Map<String, T> getByDescriptionId(ItemStack itemStack, HolderLookup.Provider registries);
 
     @Nullable
     protected Component getValueComponent(T t) {
@@ -81,33 +98,38 @@ public abstract class TooltipDescriptionsHandler<T> {
 
     protected abstract List<Component> getItemTooltipLines(T t);
 
-    @Nullable
-    protected static TranslatableContents getTranslatableContents(Component component) {
+    public static boolean modifyTranslatableContents(Component component, UnaryOperator<Component> componentReplacer, BiPredicate<TranslatableContents, UnaryOperator<Component>> contentsGatherer) {
         if (component.getContents() instanceof TranslatableContents translatableContents) {
-            return getNestedTranslatableContents(translatableContents);
-        } else if (!component.getSiblings().isEmpty()) {
-            return getTranslatableContents(component.getSiblings().getFirst());
-        } else {
-            return null;
-        }
-    }
+            if (contentsGatherer.test(translatableContents, componentReplacer)) {
+                return true;
+            } else {
+                for (int i = 0; i < translatableContents.getArgs().length; i++) {
+                    int index = i;
 
-    private static TranslatableContents getNestedTranslatableContents(TranslatableContents contents) {
-        if (contents.getArgs().length != 0 && contents.getArgs()[0] instanceof Component component
-                && component.getContents() instanceof TranslatableContents innerContents) {
-            return getNestedTranslatableContents(innerContents);
-        } else {
-            return contents;
-        }
-    }
-
-    public static Set<String> getAllTranslationKeys(List<Component> tooltipLines) {
-        return tooltipLines.stream().mapMulti((Component component, Consumer<TranslatableContents> consumer) -> {
-            TranslatableContents contents = TooltipDescriptionsHandler.getTranslatableContents(component);
-            if (contents != null) {
-                consumer.accept(contents);
+                    if (translatableContents.getArgs()[index] instanceof Component componentArg) {
+                        if (modifyTranslatableContents(componentArg, (Component componentX) -> {
+                            translatableContents.getArgs()[index] = componentX;
+                            return componentReplacer.apply(component);
+                        }, contentsGatherer)) {
+                            return true;
+                        }
+                    }
+                }
             }
-        }).map(TranslatableContents::getKey).collect(Collectors.toSet());
+        }
+
+        for (int i = 0; i < component.getSiblings().size(); i++) {
+            int index = i;
+
+            if (modifyTranslatableContents(component.getSiblings().get(index), (Component componentX) -> {
+                component.getSiblings().set(index, componentX);
+                return componentReplacer.apply(component);
+            }, contentsGatherer)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static <T> void printMissingDescriptionWarnings(ResourceKey<? extends Registry<? extends T>> registryKey, Function<Holder.Reference<T>, String> descriptionIdGetter) {
